a0b05af82e82943e821e57e5ed32878671e79206
AtheoJester23 <atheojester@gmail.com>
2025-04-14 21:27:29 +0800

    Fix Home Page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,63 @@
-export default function Home() {
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({searchParams}: {
+  searchParams: Promise<{query ?: string}>
+}) {
+
+  const query = (await searchParams).query
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {_id: 1, name: "Somebody"},
+      _id: 1,
+      description: "Testing description only...",
+      image: "https://images.unsplash.com/photo-1627773755683-dfcf2774596a?q=80&w=1452&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Music",
+      title: "Music Producing"
+    },{
+      _createdAt: new Date(),
+      views: 25,
+      author: {_id: 2, name: "Atheo"},
+      _id: 2,
+      description: "Only a Testing description",
+      image: "https://images.unsplash.com/photo-1627773755683-dfcf2774596a?q=80&w=1452&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Music",
+      title: "Music Producing"
+    }
+  ]
+
   return (
     <>
-      <p className="text-2xl">Testing</p>
+      <section className="pink_container">
+      
+        <h1 className="heading">This is just a testing text</h1>
+      
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+        </p>
+
+        <SearchForm query={query}/>
+      </section>
+
+      <section className="section_container">
+        <p className="text-semibold text-[30px] text-black">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((item: testing) => (
+              <StartupCard key={item?._id} prop={item}/>
+            ))
+          ) : (
+            <p className="no-result">No startups</p>
+          )}
+        </ul>
+      </section>
+
     </>
   );
 }
---
+++ components/StartupCard.tsx
@@ -0,0 +1,67 @@
+import { formatDate } from '@/lib/utils';
+import { EyeIcon } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react'
+import { Button } from './ui/button';
+
+const StartupCard = ({prop}: {prop: testing}) => {
+  const {_createdAt, views, author: { _id: authorId, name}, title, _id, description, image, category} = prop
+
+  console.log(image);
+
+    return (
+    <li className='startup-card shadow-xs'>
+        <div className="flex-between">
+            <p>{formatDate(_createdAt)}</p>
+        
+            <div className="flex gap-1 5">
+                <EyeIcon className='size-6 text-primary'></EyeIcon>
+                <span className='text-16-medium'>{views}</span>
+            </div>
+        </div>
+
+        <div className="flex-between mt-5 gap5">
+          <div className="flex-1">
+            <Link href={`/user/${authorId}`}>
+              <p className="text-[16px] font-bold line-clamp-1">
+                {name}
+              </p>
+            </Link>
+            <Link href={`/startup/${_id}`}>
+              <h3 className='text-[26px] text-semibold font-bold'>
+                {title}
+              </h3>
+            </Link>
+          </div>
+
+          <Link href={`/user/${authorId}`}>
+            <Image src="https://placehold.co/48x48" alt='placeholder' width={48} height={48} className='rounded-full'/>
+          </Link>
+        </div>
+
+        <Link href={`/startup/${_id}`}>
+          <p className="startup-card_desc">
+            {description}
+          </p>
+
+          <img src={image} alt="placeholder" className='startup-card_img'/>
+        </Link>
+
+        <div className="flex-between gap-3 mt-5">
+          <Link href={`/?query=${category.toLowerCase()}`}>
+            <p className='text-[16px] text-medium'>
+              {category}
+            </p>
+          </Link>
+          <Button className='startup-card_btn' asChild>
+            <Link href={`/startup/${_id}`}>
+              Details
+            </Link>
+          </Button>
+        </div>
+    </li>
+  )
+}
+
+export default StartupCard
