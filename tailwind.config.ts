ca17ddf50e58195d9edc8881e11cfb191806b70c
AtheoJester23 <atheojester@gmail.com>
2025-04-20 20:11:52 +0800

    Add Details Page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type {Config} from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 const config: Config = {
     darkMode: "class",
@@ -45,7 +46,10 @@ const config: Config = {
             },
         },
     },
-    plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+    plugins: [
+        require("tailwindcss-animate"),
+        typography,
+    ],
 };
 
 export default config;
