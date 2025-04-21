dc3bfd274d008d63e9b45ad0a22a67e7389512d3
AtheoJester23 <atheojester@gmail.com>
2025-04-21 19:22:36 +0800

    Add temporary static view count

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -3,8 +3,10 @@ import { client } from '@/sanity/lib/client'
 import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
-import React from 'react'
+import React, { Suspense } from 'react'
 import markdownit from 'markdown-it'
+import { Skeleton } from '@/components/ui/skeleton'
+import View from '@/components/View'
 
 const md = markdownit();
 
@@ -55,7 +57,11 @@ const page = async ({params}: {params: Promise<{id: string}>}) => {
 
         <hr className="divider"/>
 
-        {/* TODO: EDITOR SELECTED STARTUPS */}      
+        {/* TODO: EDITOR SELECTED STARTUPS */}
+
+        <Suspense fallback={<Skeleton className='view_skeleton'/>}>
+            <View id={id}/>
+        </Suspense>      
       </section>
     </div>
   )
