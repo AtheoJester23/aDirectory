7398a22a168fa59f4e728c9bc6beb82c38d640a6
AtheoJester23 <atheojester@gmail.com>
2025-07-16 17:55:42 +0800

    fix: live fetch missing token

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -6,10 +6,12 @@ import "server-only"
 import { defineLive } from "next-sanity";
 import { client } from './client'
 
+const token = process.env.SANITY_WRITE_TOKEN
+
 export const { sanityFetch, SanityLive } = defineLive({ 
   client: client.withConfig({ 
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
     apiVersion: 'vX' 
-  }) 
+  }),
+  serverToken: token,
+  browserToken: token
 });
